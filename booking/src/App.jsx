d5305a4ec780db3ec6aabb6e076b9717a5c7205f
ShivamSingh226 <shivamsingh226@gmail.com>
2023-10-16 18:14:09 +0530

    Adding photos functionality

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -6,6 +6,7 @@ import Layout from "./Layout";
 import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
 import { UserContextProvider } from "./UserContext";
+import Account from "./Account";
 axios.defaults.baseURL='http://localhost:4000'
 axios.defaults.withCredentials=true;
 function App() {
@@ -16,6 +17,10 @@ function App() {
       <Route index element={<IndexPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/account/:subpage?" element={<Account/>}/>
+        <Route path="/account/:subpage/:action" element={<Account/>}/>
+        
+        
       </Route>
      
 
