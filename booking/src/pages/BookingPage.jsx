0e1931e1c489bdfd2eb529ad0245ddb93b52a48b
ShivamSingh226 <shivamsingh226@gmail.com>
2023-10-31 12:44:22 +0530

    Project completed

--- booking/src/pages/BookingPage.jsx
+++ booking/src/pages/BookingPage.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-
+import axios from "axios";
+import AddressLink from "../AddressLink";
+import BookingDates from "../BookingDates";
+import PlaceGallery from "../PlaceGallery";
 export default function BookingPage(){
     const {id}=useParams();
+    const[booking,setBooking]=useState('');
+    useEffect(()=>{
+        if(id){
+            axios.get('/bookings').then(response=>{
+                const foundBooking=response.data.find(({_id})=>_id===id)
+                if(foundBooking){
+                    setBooking(foundBooking);
+                }
+            })
+        }
+    },[id]);
+    if(!booking){
+        return '';
+    }
     return(
-        <div>
-            Hello! {id}
+        <div className="my-8">
+           <h1 className="text-3xl">{booking.place.title}</h1>
+           <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
+           <div className="bg-gray-200 my-6 p-6 rounded-2xl flex items-center justify-between">
+            <div>
+            <h2 className="text-2xl mb-4">Your Booking information:</h2>
+            <BookingDates booking={booking}/>
+            </div>
+            <div className="bg-primary p-6 rounded-2xl text-white">
+               <div>Total Price</div> 
+               <div className="text-3xl">${booking.price}</div>
+            </div>
+            
+           </div>
+           <PlaceGallery place={booking.place} />
         </div>
     )
 }
--- booking/src/pages/BookingsPage.jsx
+++ booking/src/pages/BookingsPage.jsx
@@ -3,6 +3,8 @@ import AccountNav from "../AccountNav";
 import axios from "axios";
 import PlaceImage from "../PlaceImage";
 import { differenceInBusinessDays, format } from "date-fns";
+import { Link } from "react-router-dom";
+import BookingDates from "../BookingDates";
 export default function BookingsPage() {
   const [bookings, setBookings] = useState([]);
   useEffect(() => {
@@ -16,60 +18,28 @@ export default function BookingsPage() {
       <div>
         {bookings?.length > 0 &&
           bookings.map((booking) => (
-            <div className="flex gap-4 rounded-2xl overflow-hidden bg-gray-200">
+            <Link to={`/account/bookings/${booking._id}`} className="flex gap-4 mt-4 rounded-2xl overflow-hidden bg-gray-200">
               <div className="w-48">
                 <PlaceImage place={booking.place} />
               </div>
               <div className="py-3 grow pr-3">
                 <h2 className="text-xl">{booking.place.title}</h2>
-                <div className=" flex gap-2 items-center border-t border-gray-300 mt-2 py-2">
-                    <div className="flex">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-                    />
-                  </svg>
-                  {format(new Date(booking.checkIn), "yyyy-MM-dd")}
-                    </div>
-                  
-                  &rarr;
-                  <div className="flex">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-                    />
-                  </svg>
-                  {format(new Date(booking.checkOut), "yyyy-MM-dd")}
-                  </div>
-                  
-                </div>
+              
                 <div className="text-xl">
-                  {differenceInBusinessDays(
-                    new Date(booking.checkOut),
-                    new Date(booking.checkIn)
-                  )}{" "}
-                  nights | Total price: ${booking.price}
+                 <BookingDates booking={booking} className="mb-2 mt-4 text-gray-500"/>
+                 
+                  <div className="flex gap-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+</svg>
+<span className="text-2xl">
+Total price: ${booking.price}
+</span>
+
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
       </div>
     </div>
